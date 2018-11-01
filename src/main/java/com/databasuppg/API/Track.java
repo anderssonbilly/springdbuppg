40a3b6123294736fa77d31ae5d77e7309680ba17
Mikael Larsson <larsson.mikael96@gmail.com>
2018-11-01 18:39:32 +0100

    init

--- src/main/java/com/databasuppg/API/Track.java
+++ src/main/java/com/databasuppg/API/Track.java
@@ -1,4 +1,18 @@
 package com.databasuppg.API;
+import lombok.Getter;
 
 public class Track {
+    @Getter String name;
+    @Getter int duration;
+    @Getter String url;
+
+    public Track(String name, int duration, String url) {
+        this.name = name;
+        this.duration = duration;
+        this.url = url;
+    }
+
+
+
+
 }
