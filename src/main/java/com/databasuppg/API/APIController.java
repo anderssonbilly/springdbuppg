af011c0af77fecb7ae8148fb75ef892bedb7dd7e
Mikael Larsson <larsson.mikael96@gmail.com>
2018-11-03 23:16:09 +0100

    implemented getting tracks from album

--- src/main/java/com/databasuppg/API/APIController.java
+++ src/main/java/com/databasuppg/API/APIController.java
@@ -1,18 +1,14 @@
 package com.databasuppg.API;
 
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Unmarshaller;
 import java.io.*;
 import java.net.HttpURLConnection;
-import javax.xml.bind.JAXBException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.net.URL;
+import java.util.ArrayList;
 
 import com.databasuppg.config.Config;
-import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -36,12 +32,38 @@ public class APIController {
         }
     }
 
-    public Album searchAlbum(String name, int limit) throws IOException, SAXException {
 
+    public static void main(String[] args) {
+
+        Config conf = new Config();
+        APIController c = new APIController(conf.getAPIKey());
+
+
+        try {
+            ArrayList<Album> albumResults = c.searchAlbum("believe", 10);
+            for(Track track : c.getTracks(albumResults.get(0))) {
+                System.out.println(track.toString());
+            }
+
+        } catch (SAXException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public ArrayList<Album> searchAlbum(String name, int limit) throws IOException, SAXException {
+         ArrayList<Album> results = new ArrayList<>();
+
+        // use API method album.search
+        // https://www.last.fm/api/show/album.search
         URL url = new URL(root + String.format("?method=album.search&album=%s&limit=%s&api_key=%s", name, limit, this.key));
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+        // gets the nodes of the album tag
         Document doc = documentBuilder.parse(con.getInputStream());
         doc.getDocumentElement().normalize();
+
+        // gets the nodes of the album tag
         NodeList result = doc.getElementsByTagName("album");
 
         // loops through every node in album element
@@ -49,22 +71,71 @@ public class APIController {
 
             // gets the current element
             Node node = result.item(i);
-            System.out.println("\nCurrent Element :" + node.getNodeName());
 
             // gets the value if the node is of type element
             if(node.getNodeType() == Node.ELEMENT_NODE) {
                 Element element = (Element) node;
 
-                return new Album(
-                        element.getElementsByTagName("name").item(0).getTextContent(),
-                        element.getElementsByTagName("artist").item(0).getTextContent(),
-                        element.getElementsByTagName("url").item(0).getTextContent(),
-                        element.getElementsByTagName("image").item(2).getTextContent() // Three image sizes are available.
-                                                                                             //  index* 1: small, 2: medium, 3: large
+                Album temp =  new Album(
+                                element.getElementsByTagName("name").item(0).getTextContent(),
+                                element.getElementsByTagName("artist").item(0).getTextContent(),
+                                element.getElementsByTagName("url").item(0).getTextContent(),
+                                element.getElementsByTagName("image").item(2).getTextContent()  // Three image sizes are available.
+                                                                                                      //  index* 1: small, 2: medium, 3: large
                 );
+                results.add(temp);
             }
         }
-        return new Album();
+        return results;
+
     }
+
+    public ArrayList<Track> getTracks(Album album) throws IOException, SAXException {
+
+        ArrayList<Track> results = new ArrayList<>();
+        String albumName = album.getName();
+        String artistName = album.getArtist();
+
+        // use API method album.getInfo
+        // https://www.last.fm/api/show/album.getInfo
+        URL url = new URL(root + String.format("?method=album.getinfo&artist=%s&album=%s&api_key=%s",artistName, albumName, this.key));
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+        Document doc = documentBuilder.parse(con.getInputStream());
+        doc.getDocumentElement().normalize();
+
+        // gets the nodes of the tracks tag
+        NodeList result = doc.getElementsByTagName("track");
+
+        //TODO unified function for getting child elements
+        for(int i=0; i<result.getLength(); i++) {
+            Node node = result.item(i);
+
+            if(node.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) node;
+
+                Track temp = new Track(
+                        element.getElementsByTagName("name").item(0).getTextContent(),
+                        Integer.parseInt(element.getElementsByTagName("duration").item(0).getTextContent()),
+                        element.getElementsByTagName("url").item(0).getTextContent()
+                );
+
+                results.add(temp);
+
+            }
+
+
+        }
+
+       return results;
+    }
+
+    //TODO implement this to prevent redundant code.
+    private Object getChildElements(String tag) {
+
+        return Object;
+    }
+
+
 
 }
