0b9334cd7d13d482a354af95f25c461c20d82e42
Mikael Larsson <mikael@debian.net>
2018-11-07 20:12:20 +0100

    returns album object

--- src/main/java/com/databasuppg/API/APIController.java
+++ src/main/java/com/databasuppg/API/APIController.java
@@ -1,30 +1,33 @@
 package com.databasuppg.API;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.net.URL;
-import java.util.ArrayList;
-
-import com.databasuppg.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+
+
 public class APIController {
 
-    private String key;
+    @Value("${lastfm.api.key}") private String key;
+
     private String root = "http://ws.audioscrobbler.com/2.0/";
     private DocumentBuilder documentBuilder;
 
 
-    APIController(String key) {
-        this.key = key;
-
+    public APIController() {
         try {
             this.documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
         } catch(ParserConfigurationException e) {
@@ -33,109 +36,110 @@ public class APIController {
     }
 
 
+    // testing
     public static void main(String[] args) {
+        APIController api = new APIController();
 
-        Config conf = new Config();
-        APIController c = new APIController(conf.getAPIKey());
+        ArrayList<Album> results = api.getAlbum("believe", 20);
 
+        for (Album album : results) {
+            System.out.println(album.toString());
+            System.out.println("+---------------------+");
+        }
+
+
+    }
+
+    public ArrayList<Album> getAlbum(String albumName, int limit) {
+
+        ArrayList<Album> results = new ArrayList<>();
 
         try {
-            ArrayList<Album> albumResults = c.searchAlbum("believe", 10);
-            for(Track track : c.getTracks(albumResults.get(0))) {
-                System.out.println(track.toString());
+            Document albumResults = searchAlbum(albumName, limit);
+            albumResults.normalize();
+
+            Element element = albumResults.getDocumentElement();
+            NodeList nodes = element.getElementsByTagName("album");
+
+
+            for(int i=0; i<nodes.getLength(); i++) {
+                Node node = nodes.item(i);
+
+                if(node.getNodeType() == Node.ELEMENT_NODE) {
+
+                    String name = element.getElementsByTagName("name").item(0).getTextContent();
+                    String artist = element.getElementsByTagName("artist").item(0).getTextContent();
+                    String url = element.getElementsByTagName("url").item(0).getTextContent();
+                    String image = element.getElementsByTagName("image").item(1).getTextContent();
+                    ArrayList<Track> tracks = getTrack(name, artist);
+
+                    Document temp = getAlbumInfo(name, artist);
+                    temp.normalize();
+
+                    results.add(new Album(name, artist, url, image, tracks));
+
+                }
             }
 
-        } catch (SAXException | IOException e) {
+
+        } catch(IOException | SAXException e) {
             e.printStackTrace();
         }
+
+
+        return results;
+    }
+
+    private ArrayList<Track> getTrack(String albumName, String artistName) throws IOException, SAXException {
+
+        ArrayList<Track> result = new ArrayList<>();
+
+        Document doc = getAlbumInfo(albumName, artistName);
+        doc.normalize();
+
+        Element element = doc.getDocumentElement();
+        NodeList nodes = element.getElementsByTagName("track");
+
+        for(int i=0; i<nodes.getLength(); i++) {
+            Node node = nodes.item(i);
+
+            if(node.getNodeType() == node.ELEMENT_NODE) {
+               Element nodeElement = (Element) node;
+
+               String name = nodeElement.getElementsByTagName("name").item(0).getTextContent();
+               int duration = Integer.parseInt(nodeElement.getElementsByTagName("duration").item(0).getTextContent());
+               String url = nodeElement.getElementsByTagName("url").item(0).getTextContent();
+
+               result.add(new Track(name, duration, url));
+
+            }
+
+
+
+        }
+
+        return result;
+
     }
 
 
-    public ArrayList<Album> searchAlbum(String name, int limit) throws IOException, SAXException {
-         ArrayList<Album> results = new ArrayList<>();
-
-        // use API method album.search
-        // https://www.last.fm/api/show/album.search
+    //TODO Implement page variable
+    private Document searchAlbum(String name, int limit) throws IOException, SAXException {
         URL url = new URL(root + String.format("?method=album.search&album=%s&limit=%s&api_key=%s", name, limit, this.key));
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
-
-        // gets the nodes of the album tag
-        Document doc = documentBuilder.parse(con.getInputStream());
-        doc.getDocumentElement().normalize();
-
-        // gets the nodes of the album tag
-        NodeList result = doc.getElementsByTagName("album");
-
-        // loops through every node in album element
-        for(int i=0; i<result.getLength(); i++) {
-
-            // gets the current element
-            Node node = result.item(i);
-
-            // gets the value if the node is of type element
-            if(node.getNodeType() == Node.ELEMENT_NODE) {
-                Element element = (Element) node;
-
-                Album temp =  new Album(
-                                element.getElementsByTagName("name").item(0).getTextContent(),
-                                element.getElementsByTagName("artist").item(0).getTextContent(),
-                                element.getElementsByTagName("url").item(0).getTextContent(),
-                                element.getElementsByTagName("image").item(2).getTextContent()  // Three image sizes are available.
-                                                                                                      //  index* 1: small, 2: medium, 3: large
-                );
-                results.add(temp);
-            }
-        }
-        return results;
+        return documentBuilder.parse(con.getInputStream());
 
     }
 
-    public ArrayList<Track> getTracks(Album album) throws IOException, SAXException {
-
-        ArrayList<Track> results = new ArrayList<>();
-        String albumName = album.getName();
-        String artistName = album.getArtist();
-
-        // use API method album.getInfo
-        // https://www.last.fm/api/show/album.getInfo
-        URL url = new URL(root + String.format("?method=album.getinfo&artist=%s&album=%s&api_key=%s",artistName, albumName, this.key));
+    private Document getAlbumInfo(String album, String artist) throws IOException, SAXException {
+        URL url = new URL(root + String.format("?method=album.getinfo&api_key=%s&artist=%s&album=%s", this.key, artist, album));
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
-
-        Document doc = documentBuilder.parse(con.getInputStream());
-        doc.getDocumentElement().normalize();
-
-        // gets the nodes of the tracks tag
-        NodeList result = doc.getElementsByTagName("track");
-
-        //TODO unified function for getting child elements
-        for(int i=0; i<result.getLength(); i++) {
-            Node node = result.item(i);
-
-            if(node.getNodeType() == Node.ELEMENT_NODE) {
-                Element element = (Element) node;
-
-                Track temp = new Track(
-                        element.getElementsByTagName("name").item(0).getTextContent(),
-                        Integer.parseInt(element.getElementsByTagName("duration").item(0).getTextContent()),
-                        element.getElementsByTagName("url").item(0).getTextContent()
-                );
-
-                results.add(temp);
-
-            }
-
-
-        }
-
-       return results;
+        return documentBuilder.parse(con.getInputStream());
     }
-
-    //TODO implement this to prevent redundant code.
-//    private Object getChildElements(String tag) {
-
-//        return Object;
-//    }
 
 
 
 }
+
+
+
