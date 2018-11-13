d4959f06238c2d6a5990981653d62e0b99414a46
Mikael Larsson <mikael@debian.net>
2018-11-13 21:21:09 +0100

    Added getTracksFromAlbum, getTracksFromPlaylist

--- src/main/java/com/databasuppg/springdb/controller/CollectionController.java
+++ src/main/java/com/databasuppg/springdb/controller/CollectionController.java
@@ -23,8 +23,11 @@ import org.springframework.web.servlet.ModelAndView;
 import com.databasuppg.API.APIController;
 import com.databasuppg.API.Track;
 
+
+
 @Controller
 public class CollectionController {
+
 
 	@Autowired
 	private APIController apiController;
@@ -46,7 +49,7 @@ public class CollectionController {
 	}
 
 	public ArrayList<Track> getTracksFromUser(String username) {
-		Connection conn;
+	    Connection conn;
 		ResultSet result;
 		CallableStatement statement;
         ArrayList<Track> tracks = new ArrayList<>();
@@ -63,7 +66,6 @@ public class CollectionController {
 
 
 			while(result.next()) {
-                System.out.println(result.getString("artist"));
                 tracks.add(new Track(
                         result.getString("title"),
                         result.getString("artist"),
@@ -78,9 +80,70 @@ public class CollectionController {
 		return tracks;
 	}
 
-	public static void main(String[] args) {
-	}
-	
+    public ArrayList<Track> getTracksFromPlaylist(int playlistId) {
+	    Connection conn;
+        ResultSet result;
+        CallableStatement statement;
+        ArrayList<Track> tracks = new ArrayList<>();
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://217.208.107.210:3301/dbuppgift", "uppgift", "password");
+            String query = "CALL getSongsFromPlaylist(?)";
+            statement = conn.prepareCall(query);
+            statement.setInt(1, playlistId);
+
+            result = statement.executeQuery();
+
+
+
+            while(result.next()) {
+                System.out.println(result.getString("artist"));
+                tracks.add(new Track(
+                        result.getString("title"),
+                        result.getString("artist"),
+                        result.getInt("length")));
+
+            }
+
+        } catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return tracks;
+    }
+
+    public ArrayList<Track> getSongsFromAlbum(int albumId) {
+
+        Connection conn;
+        ResultSet result;
+        CallableStatement statement;
+        ArrayList<Track> tracks = new ArrayList<>();
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://217.208.107.210:3301/dbuppgift", "uppgift", "password");
+
+            String query = "CALL getSongsFromAlbum(?)";
+            statement = conn.prepareCall(query);
+            statement.setInt(1, albumId);
+
+            result = statement.executeQuery();
+
+            while(result.next()) {
+                System.out.println(result.getString("artist"));
+                tracks.add(new Track(
+                        result.getString("title"),
+                        result.getString("artist"),
+                        result.getInt("length")));
+
+            }
+
+        } catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return tracks;
+    }
+
 	@RolesAllowed("USER")
 	@RequestMapping("/collection")
 	public ModelAndView collection(Model model) {
