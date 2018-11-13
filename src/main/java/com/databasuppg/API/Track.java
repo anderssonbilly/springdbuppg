9c08081ca097a97b12722c9add12264c7c6bf968
Mikael Larsson <mikael@debian.net>
2018-11-13 20:39:21 +0100

    Added artist

--- src/main/java/com/databasuppg/API/Track.java
+++ src/main/java/com/databasuppg/API/Track.java
@@ -3,16 +3,24 @@ import lombok.Getter;
 
 public class Track {
     @Getter String name;
+    @Getter String artist;
     @Getter int duration;
     @Getter String url;
 
-    public Track(String name, int duration, String url) {
+    public Track(String name, String artist, int duration, String url) {
         this.name = name;
         this.duration = duration;
         this.url = url;
     }
 
+    public Track(String name, String artist, int duration) {
+        this.name = name;
+        this.artist = artist;
+        this.duration = duration;
+    }
+
     public Track(){}
+
 
     @Override
     public String toString() {
